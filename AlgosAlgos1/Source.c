f9beabd9dd6c8acb3d2640f36f84d2ba3493c7cc
Evaldas Paulauskas <crowjamboo2@mail.com>
2019-02-04 09:16:15 +0200

    Readability improvements

--- AlgosAlgos1/Source.c
+++ AlgosAlgos1/Source.c
@@ -12,29 +12,29 @@ EVALDAS PAULAUAS PI18E
 
 int main() {
 
-	int number, originalNumber, remainder, result = 0, n = 0;
+	int number, tempNumber, remainder, result = 0, counter = 0;
 
 	printf("Enter an integer: ");
 	scanf_s("%d", &number);
 
-	originalNumber = number;
+	tempNumber = number;
 
-	while (originalNumber != 0)
+	while (tempNumber != 0)
 	{
-		originalNumber /= 10;
-		++n;
+		tempNumber /= 10;
+		++counter;
 	}
 
-	originalNumber = number;
+	tempNumber = number;
 
-	while (originalNumber != 0)
+	while (tempNumber != 0)
 	{
-		remainder = originalNumber % 10;
+		remainder = tempNumber % 10;
 		printf("\n remainder %d", remainder);
-		result += pow(remainder, n);
+		result += pow(remainder, counter);
 		printf("\n result %d", result);
-		originalNumber /= 10;
-		printf("\n original number %d", originalNumber);
+		tempNumber /= 10;
+		printf("\n temp number %d \n", tempNumber);
 	}
 
 	if (result == number)
@@ -46,19 +46,3 @@ int main() {
 
 	return 0;
 }
-
-/*
-Pseudo code:
-
-READ n 
-temp=n 
-sum=0 
-
-WHILE n>=0 
-sum=sum+(n%10)*(n%10)*(n%10) n=n/10 
-ENDWHILE 
-
-IF sum==temp WRITE "NUMBER IS AN ARMSTRONG NUMBER"
-ELSE WRITE "NUMBER IS NOT AN ARMSTRONG NUMBER"
-
-*/
